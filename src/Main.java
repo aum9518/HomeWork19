f418018c6b4a67bd463724d54005e80669c0adf1
aum9518 <abduganiuulumanasbek@gmail.com>
2023-02-23 11:55:56 +0600

    add HomeWork19

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SolarSystem solarSystem = SolarSystem.valueOf(new Scanner(System.in).nextLine());
+        switch (solarSystem){
+            case MARS -> System.out.println(SolarSystem.MARS.getFutures());
+            case EARTH -> System.out.println(SolarSystem.EARTH.getFutures());
+            case VENUS -> System.out.println(SolarSystem.VENUS.getFutures());
+            case SATURN -> System.out.println(SolarSystem.SATURN.getFutures());
+            case URANUS -> System.out.println(SolarSystem.URANUS.getFutures());
+            case JUPITER -> System.out.println(SolarSystem.JUPITER.getFutures());
+            case MERCURY -> System.out.println(SolarSystem.MERCURY.getFutures());
+            case NEPTUNE -> System.out.println(SolarSystem.NEPTUNE.getFutures());
+            default -> System.out.println("Invalid command");
+        }
     }
 }
